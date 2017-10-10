040096d69ece1f24bfd13bc8ff51556f5316351a
Robin Dickson <robindickson@gmail.com>
2017-10-10 17:05:18 +0200

    Add horizontal and vertical centering

--- App.js
+++ App.js
@@ -17,21 +17,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // flexDirection defaults to 'column'
-    // flexDirection: 'row',
+    flexDirection: 'row',
     // justifyContent: 'flex-start',
-    // justifyContent: 'center',
+    justifyContent: 'center',
     // justifyContent: 'flex-end',
     // justifyContent: 'space-between',
     // justifyContent: 'space-around',
     // alignItems: 'flex-start',
-    // alignItems: 'center',
+    alignItems: 'center',
     // alignItems: 'flex-end',
     // with stretch it will stretch as long as there is no set width
     // alignItems: 'stretch',
   },
   box: {
     height: 50,
-    // width: 50,
+    width: 50,
     backgroundColor: '#e76e63',
     margin: 10,
   }
